68d7ec78caa8ff8909c698b4e73d2fb827e6f4e4
3milk <429407748@qq.com>
2019-05-24 14:24:57 +0800

    Add synchronization to the file system to ensure that Read and Write operations on each file are atomic

---
+++ filesys/FileAccessController.cc
@@ -0,0 +1,123 @@
+#include "copyright.h"
+#include "synch.h"
+#include "system.h"
+#include "FileAccessController.h"
+
+FileAccessController::FileAccessController()
+{
+
+}
+
+
+FileAccessController::~FileAccessController()
+{
+
+}
+
+
+void
+FileAccessController::open(int hdr)
+{
+	// assume that open a file, not a directory
+	ASSERT(hdr >= 0 && hdr < NumSectors);
+	IntStatus oldLevel = interrupt->SetLevel(IntOff);	// disable interrupts
+	if(facbs[hdr].rwlock == NULL) {
+		facbs[hdr].rwlock = new RWLock();
+	}
+	(void) interrupt->SetLevel(oldLevel);	// re-enable interrupts
+}
+
+// call by FileSystem::Remove
+bool
+FileAccessController::remove(int hdr)
+{
+	bool canRemove = false;
+	ASSERT(hdr >= 0 && hdr < NumSectors);
+	IntStatus oldLevel = interrupt->SetLevel(IntOff);	// disable interrupts
+	facbs[hdr].toRemove = true;
+	if(facbs[hdr].referenceNum == 0)
+	{
+		canRemove = true;
+	}
+	(void) interrupt->SetLevel(oldLevel);	// re-enable interrupts
+	return canRemove;
+}
+
+// call by OpenFile::~OpenFile()
+bool
+FileAccessController::checkRemove(int hdr)
+{
+	bool canRemove = false;
+	ASSERT(hdr >= 0 && hdr < NumSectors);
+	IntStatus oldLevel = interrupt->SetLevel(IntOff);	// disable interrupts
+	if(facbs[hdr].referenceNum == 0 && facbs[hdr].toRemove)
+	{
+		canRemove = true;
+	}
+	(void) interrupt->SetLevel(oldLevel);	// re-enable interrupts
+	return canRemove;
+}
+
+void
+FileAccessController::finishRemove(int hdr)
+{
+	ASSERT(hdr >= 0 && hdr < NumSectors);
+	IntStatus oldLevel = interrupt->SetLevel(IntOff);	// disable interrupts
+	facbs[hdr].toRemove = false;
+	(void) interrupt->SetLevel(oldLevel);	// re-enable interrupts
+}
+
+
+bool
+FileAccessController::getToRemove(int hdr)
+{
+	ASSERT(hdr >= 0 && hdr < NumSectors);
+	return facbs[hdr].toRemove;
+}
+
+void
+FileAccessController::rlock(int hdr)
+{
+	ASSERT(hdr >= 0 && hdr < NumSectors);
+	IntStatus oldLevel = interrupt->SetLevel(IntOff);	// disable interrupts
+	++facbs[hdr].referenceNum;
+	facbs[hdr].rwlock->rlock();
+
+	(void) interrupt->SetLevel(oldLevel);	// re-enable interrupts
+}
+
+
+void
+FileAccessController::runlock(int hdr)
+{
+	ASSERT(hdr >= 0 && hdr < NumSectors);
+	IntStatus oldLevel = interrupt->SetLevel(IntOff);	// disable interrupts
+	--facbs[hdr].referenceNum;
+	facbs[hdr].rwlock->runlock();
+
+	(void) interrupt->SetLevel(oldLevel);	// re-enable interrupts
+}
+
+
+void
+FileAccessController::wlock(int hdr)
+{
+	ASSERT(hdr >= 0 && hdr < NumSectors);
+	IntStatus oldLevel = interrupt->SetLevel(IntOff);	// disable interrupts
+	++facbs[hdr].referenceNum;
+	facbs[hdr].rwlock->wlock();
+
+	(void) interrupt->SetLevel(oldLevel);	// re-enable interrupts
+}
+
+
+void
+FileAccessController::wunlock(int hdr)
+{
+	ASSERT(hdr >= 0 && hdr < NumSectors);
+	IntStatus oldLevel = interrupt->SetLevel(IntOff);	// disable interrupts
+	--facbs[hdr].referenceNum;
+	facbs[hdr].rwlock->wunlock();
+
+	(void) interrupt->SetLevel(oldLevel);	// re-enable interrupts
+}
---
+++ filesys/FileAccessController.h
@@ -0,0 +1,51 @@
+// FileAccessController.h
+//	1. synchronize readers and writers when they request to access the same file
+//  2. delete the file if and only if all reference proocesses finish
+//  TODO not implement remove the file/derectory atomically
+
+#ifndef FAC_H
+#define FAC_H
+
+#include "copyright.h"
+#include "utility.h"
+#include "synch.h"
+#include "RWLock.h"
+
+//#define FILESYS // TODO add to Makefile, not here
+#ifdef FILESYS
+struct FileAccCtrlBlock {
+	int referenceNum;
+	RWLock* rwlock;
+	bool toRemove;	// set TRUE when File::Remove was called (consider derectory remove?!)
+		// check this variable when some file close (~OpenFile())
+	FileAccCtrlBlock(): referenceNum(0), rwlock(NULL), toRemove(false) {}
+	~FileAccCtrlBlock()
+	{
+		if(rwlock != NULL)
+			delete rwlock;
+	}
+};
+
+class FileAccessController {
+public:
+	FileAccessController();
+	~FileAccessController();
+
+	void open(int hdr);
+	bool remove(int hdr);		// called by FileSystem::Remove
+	bool checkRemove(int hdr);	// called by OpenFile::~OpenFile
+	void finishRemove(int hdr);
+	bool getToRemove(int hdr);	// called by FileSystem::Open
+	void rlock(int hdr);
+	void runlock(int hdr);
+	void wlock(int hdr);
+	void wunlock(int hdr);
+private:
+	FileAccCtrlBlock facbs[NumSectors];
+};
+
+
+#endif //FILESYS
+
+#endif //FAC
+
